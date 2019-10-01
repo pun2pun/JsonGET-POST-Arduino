aba69a8412d2fdb3e45163f1e2b102b24c6576ec
pun2pun <35132090+pun2pun@users.noreply.github.com>
2019-10-01 21:40:27 +0700

    Add files via upload

---
+++ Get_Json.ino
@@ -0,0 +1,62 @@
+//Get Json
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#define ON LOW
+#define OFF HIGH
+// WiFi Parameters
+const char* ssid = "HUAWEI Y6II";
+const char* password = "bbbbbbbb";
+int R = D1;
+String email ;
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  pinMode(R,OUTPUT);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+}
+
+void loop() {
+  // Check WiFi Status
+  Serial.println(WiFi.status());
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;  //Object of class HTTPClient
+    http.begin("http://iot.data.ubu.ac.th/api/get/lated/SCADAProject");
+    int httpCode = http.GET();
+    //Check the returning code                                                                  
+    if (httpCode > 0) {
+      // Get the request response payload
+      const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
+     // const size_t bufferSize_2 = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
+      DynamicJsonBuffer jsonBuffer(bufferSize);
+      JsonObject& root = jsonBuffer.parseObject(http.getString());
+      int id = root["id"]; // 1
+      const char* names = root["name"]; // "Leanne Graham"
+      String username = root["content"]; // "Bret"
+      String email = root["content"]["Lamp 1"]; // "Sincere@april.biz"
+      Serial.print("Name:");
+      Serial.println(names);
+      Serial.print("Username:");
+      Serial.println(username);
+      Serial.print("Email:"); 
+      Serial.println(email);
+      //Serial.println();
+      if (email == "1"){
+      digitalWrite(R,ON);
+      Serial.println("ON");
+      }
+      else 
+      {digitalWrite(R,OFF);
+      Serial.println("OFF");} 
+      // Delay
+    }
+    http.end();   //Close connection
+  }
+
+  delay(10);
+}
+
---
+++ Post_Json.ino
@@ -0,0 +1,43 @@
+//Post Json 
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+void setup() {
+  Serial.begin(115200);                            //Serial connection
+  WiFi.begin("UU", "1212312121");   //WiFi connection
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+    delay(500);
+    Serial.println("Waiting for connection");
+ }
+} 
+
+void loop() {
+  int sensor_value = 45;
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+  String json = "{\"sensor\":\"gps\",\"value\":\""+ String(sensor_value) +"\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
+  DynamicJsonBuffer  jsonBuffer;
+  char postMessage[300]; 
+  JsonObject& root = jsonBuffer.createObject();
+  JsonObject& Jarray = jsonBuffer.parseObject(json); 
+  root["name"] = "lll";
+  root["content"] = Jarray;
+  root.printTo(postMessage,sizeof(postMessage));
+  Serial.print(postMessage);
+  
+//----------------------------------------------------------
+         HTTPClient http;
+         http.begin("http://iot.data.ubu.ac.th/api/save" );      
+         http.addHeader("Content-Type", "application/json");
+         int httpCode = http.POST(postMessage);
+
+    String payload = http.getString();                                        //Get the response payload
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(payload);    //Print request response payload
+    http.end();  //Close connection
+  }
+ else {
+    Serial.println("Error in WiFi connection");
+  }
+  delay(3000);  //Send a request every 30 seconds
+}
